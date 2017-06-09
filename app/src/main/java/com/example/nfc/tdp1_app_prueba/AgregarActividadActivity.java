950b9f1d3a4ce2ad0e9f1fcd5020045c3e33cf71
juanmafc <juanmafc25@gmail.com>
2017-06-09 15:05:57 -0300

    Agrego un Spinner (Combo Box) para la prioridad

--- app/src/main/java/com/example/nfc/tdp1_app_prueba/AgregarActividadActivity.java
+++ app/src/main/java/com/example/nfc/tdp1_app_prueba/AgregarActividadActivity.java
@@ -1,9 +1,12 @@
 package com.example.nfc.tdp1_app_prueba;
 
+import android.os.StrictMode;
 import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.HashMap;
@@ -21,6 +24,11 @@ public class AgregarActividadActivity extends AppCompatActivity {
         //Deberia inicializarlo aca?, en el onCreate?
         this.textosFechas.put(R.id.buttonInicioActividad, (TextView) findViewById(R.id.textViewInicioActividad) );
         this.textosFechas.put(R.id.buttonFinActividad, (TextView) findViewById(R.id.textViewFinActividad) );
+
+        String[] prioridades = new String[] {"ALTA", "MEDIA", "BAJA"};
+        Spinner spinnerPrioridades = (Spinner) findViewById(R.id.spinnerPrioridades);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, prioridades);
+        spinnerPrioridades.setAdapter(adapter);
     }
 
 
