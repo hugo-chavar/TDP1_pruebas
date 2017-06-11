ddda7a6a6188487a3610a91b038ab7d08329a8a5
juanmafc <juanmafc25@gmail.com>
2017-06-11 11:21:54 -0300

    Objetivo tiene lista de actividades y redefino toString()

--- app/src/main/java/com/example/nfc/tdp1_app_prueba/Objetivo.java
+++ app/src/main/java/com/example/nfc/tdp1_app_prueba/Objetivo.java
@@ -1,5 +1,7 @@
 package com.example.nfc.tdp1_app_prueba;
 
+import java.util.LinkedList;
+
 /**
  * Created by User on 11/06/2017.
  */
@@ -7,12 +9,21 @@ package com.example.nfc.tdp1_app_prueba;
 public class Objetivo {
 
     private String nombre;
+    private LinkedList<Actividad> actividades;
 
     public Objetivo(String nombre) {
         this.nombre = nombre;
     }
 
     public String getNombre() {
+        return this.nombre;
+    }
+
+    public void agregarActividad(Actividad actividad){
+        this.actividades.add(actividad);
+    }
+
+    public String toString() {
         return this.nombre;
     }
 }
