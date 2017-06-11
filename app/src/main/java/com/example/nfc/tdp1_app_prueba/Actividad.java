0caa30f339af2e715639a0b8bfe9fe050d1a7135
juanmafc <juanmafc25@gmail.com>
2017-06-11 11:22:22 -0300

    Agrego clase Actividad

---
+++ app/src/main/java/com/example/nfc/tdp1_app_prueba/Actividad.java
@@ -0,0 +1,15 @@
+package com.example.nfc.tdp1_app_prueba;
+
+/**
+ * Created by User on 11/06/2017.
+ */
+
+public class Actividad {
+
+    private String nombre;
+
+    public Actividad(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
