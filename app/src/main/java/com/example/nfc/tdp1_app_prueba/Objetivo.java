ab3e1fd76c88f0cdbe127c63655bbfb6027c2ac6
juanmafc <juanmafc25@gmail.com>
2017-06-11 00:43:45 -0300

    Clase objetivo que solo guarda nombre

---
+++ app/src/main/java/com/example/nfc/tdp1_app_prueba/Objetivo.java
@@ -0,0 +1,18 @@
+package com.example.nfc.tdp1_app_prueba;
+
+/**
+ * Created by User on 11/06/2017.
+ */
+
+public class Objetivo {
+
+    private String nombre;
+
+    public Objetivo(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+}
